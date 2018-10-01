0e0a267f0554cca879244dfdb9ef368044f28518
Eric Reeves <leagues58@gmail.com>
2018-10-01 18:10:08 -0400

    added socket.io to project

--- web/ball_thirty/routes/index.js
+++ web/ball_thirty/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http').Server(router);
+var io = require('socket.io')(http);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'ball:30 System' });
 });
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+
 
 module.exports = router;
