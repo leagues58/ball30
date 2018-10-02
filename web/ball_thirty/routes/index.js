c518e6058ee1eb44d0507328c0eee14b6e03407e
Eric Reeves <leagues58@gmail.com>
2018-10-02 09:10:02 -0400

    Got websocket mostly working

--- web/ball_thirty/routes/index.js
+++ web/ball_thirty/routes/index.js
@@ -5,13 +5,10 @@ var io = require('socket.io')(http);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ball:30 System' });
+  //res.render('index', { title: 'ball:30 System' });
+  res.io.emit("socketToMe", "index");
+  res.send('respond with a resource.');
 });
-
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
-
 
 
 module.exports = router;
